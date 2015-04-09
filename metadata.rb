b822729b87a1d55d52065043a689d33a45baae2f
Philipp H <phil@hellmi.de>
2015-04-09 14:07:55 +0200

    Changed Maintainer, disabled Chef Solo foodcritic rule

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'monitor'
-maintainer 'Sean Porter Consulting'
-maintainer_email 'portertech@gmail.com'
+maintainer 'Philipp H'
+maintainer_email 'phil@hellmi.de'
 license 'Apache 2.0'
 description 'A cookbook for monitoring services, using Sensu, a monitoring framework.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
