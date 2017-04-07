539e8b305c7950ce9be6b3542858eae6db06a4d3
Patrick Robinson <autarch@outlook.com>
2017-04-07 07:31:00 +0000

    Updated logs plugin & verified

--- attributes/plugins.rb
+++ attributes/plugins.rb
@@ -9,6 +9,6 @@ default['monitor']['sensu_plugins']['disk-checks'] = '2.0.1'
 default['monitor']['sensu_plugins']['filesystem-checks'] = '0.2.0'
 default['monitor']['sensu_plugins']['vmstats'] = '1.0.0'
 default['monitor']['sensu_plugins']['io-checks'] = '1.0.0'
-default['monitor']['sensu_plugins']['logs'] = '0.0.4'
+default['monitor']['sensu_plugins']['logs'] = '1.0.0'
 
 default['monitor']['sensu_optional_plugins']['slack'] = '1.0.0'
