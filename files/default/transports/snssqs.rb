3f502c80fe7a0a08fc7cbd7af3007ac0d035ad18
Patrick Robinson <autarch@outlook.com>
2017-04-26 13:01:57 +0000

    Allow snssqs to handle non-raw SNS Messages

--- files/default/transports/snssqs.rb
+++ files/default/transports/snssqs.rb
@@ -109,12 +109,10 @@ module Sensu
           do_all_the_time {
             EM::Iterator.new(receive_messages, 10).each do |msg, iter|
               statsd_time("sqs.#{@settings[:consuming_sqs_queue_url]}.process_timing") {
-                if msg.key?("message_attributes")
-                  if msg.message_attributes[PIPE_STR].string_value == KEEPALIVES_STR
-                    @keepalives_callback.call(msg, msg.body)
-                  else
-                    @results_callback.call(msg, msg.body)
-                  end
+                if msg.message_attributes[PIPE_STR].string_value == KEEPALIVES_STR
+                  @keepalives_callback.call(msg, msg.body)
+                else
+                  @results_callback.call(msg, msg.body)
                 end
               }
               iter.next
@@ -186,7 +184,19 @@ module Sensu
             wait_time_seconds: @settings[:wait_time_seconds],
             max_number_of_messages: @settings[:max_number_of_messages],
           )
-          resp.messages
+          resp.messages.map do |msg|
+            if !msg.key? 'message_attributes'
+              tmp_body = JSON.parse msg.body
+              msg.body = tmp_body['Message']
+              msg.message_attributes = {}
+              tmp_body['MessageAttributes'].each do |name, value|
+                msg.message_attributes[name] = Aws::SQS::Types::MessageAttributeValue.new
+                msg.message_attributes[name].string_value = value['Value']
+                msg.message_attributes[name].data_type = 'String'
+              end
+            end
+            msg
+          end
         rescue Aws::SQS::Errors::ServiceError => e
           self.logger.info(e)
         end
