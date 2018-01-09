5e9e8c37a4bc05ddaf5296630b354d358dcbce46
Philipp H <phil@hellmi.de>
2018-01-09 11:31:20 +0100

    Change default keepalive thresholds to 300/900

--- recipes/client.rb
+++ recipes/client.rb
@@ -44,6 +44,11 @@ client_attributes['safe_mode'] = node['monitor']['safe_mode']
 client_attributes['standalone_mode'] = node['monitor']['standalone_mode']
 client_attributes['transport'] = node['monitor']['transport']
 
+client_attributes['keepalive'] ||= {}
+client_attributes['keepalive']['thresholds'] ||= {}
+client_attributes['keepalive']['thresholds']['warning'] ||= 300
+client_attributes['keepalive']['thresholds']['critical'] ||= 900
+
 client_attributes['influxdb'] ||= {}
 client_attributes['influxdb']['tags'] ||= {}
 
