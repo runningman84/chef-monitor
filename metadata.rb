59116eac868b77b0d73bd3f4d51d8d74e7b6d80d
Patrick Robinson <autarch@outlook.com>
2018-03-05 11:45:47 +0000

    Changed release

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license 'Apache-2.0'
 description 'A cookbook for monitoring services, using Sensu, a monitoring framework.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.9.0'
-chef_version '~> 13'
+chef_version '~> 14'
 
 issues_url 'https://github.com/runningman84/chef-monitor/issues'
 source_url 'https://github.com/runningman84/chef-monitor'
